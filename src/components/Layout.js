baf19f0b4d5c23d250d767ae03232e29dc1fd5f2
Uduak Essien <acushla4real@gmail.com>
2020-09-01 10:47:27 +0100

    initial setup with starter project

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react"
+// import "../css/main.css"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      { children }
+    </>
+  )
+}
+
+export default Layout
