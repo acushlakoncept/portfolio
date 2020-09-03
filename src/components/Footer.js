45d94ad4c795915788d69554cccb151e22f29817
Uduak Essien <acushla4real@gmail.com>
2020-09-03 10:44:04 +0100

    add footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass="footer-links"></SocialLinks>
+        <h4>
+          copyright&copy; {new Date().getFullYear()}
+          <span>WebDav</span> all rights reserved
+        </h4>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
