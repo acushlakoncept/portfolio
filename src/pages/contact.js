858bc338f0a0e5c9bbc25f9eb334b2010914db28
Uduak Essien <acushla4real@gmail.com>
2020-09-03 12:26:00 +0100

    add contact page connect to formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,21 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return <Layout>
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>get in touch</h3>
+        <form action="https://formspree.io/xvowjjkq" method="POST">
+          <div className="form-group">
+            <input type="text" name="name" placeholder="name" className="form-control" />
+            <input type="email" name="email" placeholder="email" className="form-control" />
+            <textarea name="message" rows="5" placeholder="message" className="form-control" />
+          </div>
+          <button type="submit" className="submit-btn btn">submit here</button>
+        </form>
+      </article>
+    </section>
+  </Layout>
 }
 
 export default contact
