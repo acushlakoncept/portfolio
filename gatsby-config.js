7066a0a7b844402a80ddf0902f16b952dc58a810
Uduak Essien <acushla4real@gmail.com>
2020-09-03 12:08:19 +0100

    add about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,11 +30,9 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        //   contentTypes : `jobs`, `projects`, `blogs`,
-        //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
         contentTypes: [`jobs`, `projects`, `blogs`],
-        singleTypes: [],
+        singleTypes: [`about`],
       },
     },
     // {
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,52 @@
-import React from "react"
+ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+const About = ({
+  data: {
+  about: {nodes}, 
+  }
+}) => {
+  const {title, info, stack, image} = nodes[0]
+  return <Layout>
+    <section className="about-page">
+      <div className="section-center about-center">
+        <Image fluid={image.childImageSharp.fluid} className="about-img" />
+        <article className="about-text">
+          <Title title={title}/>
+          <p>{info}</p>
+          <div className="about-stack">
+            {stack.map(item => {
+              return <span key={item.id}>{item.title}</span>
+            })}
+          </div>
+        </article>
+      </div>
+    </section>
+  </Layout>
 }
+
+export const query = graphql`
+  {
+    about:allStrapiAbout {
+      nodes {
+        title
+        stack {
+          id
+          title
+        }
+        info
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default About
