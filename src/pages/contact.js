84d815fa25586148f75486dd03d00fce1baf0f10
Uduak Essien <acushla4real@gmail.com>
2020-09-26 22:50:25 +0100

    add react-google-recaptcha

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import ReCAPTCHA from "react-google-recaptcha";
+
+const recaptchaRef = React.createRef();
+
+function onChange(value) {
+  console.log("Captcha value:", value)
+}
 
 const contact = () => {
   return <Layout>
@@ -8,12 +15,18 @@ const contact = () => {
     <section className="contact-page">
       <article className="contact-form">
         <h3>get in touch</h3>
-        <form action="https://formspree.io/xvowjjkq" method="POST">
+        <form action="https://formspree.io/xvowjjkq"  method="POST" onSubmit={() => { recaptchaRef.current.execute() }}>
           <div className="form-group">
             <input type="text" name="name" placeholder="name" className="form-control" />
             <input type="email" name="email" placeholder="email" className="form-control" />
             <textarea name="message" rows="5" placeholder="message" className="form-control" />
           </div>
+          <ReCAPTCHA
+              ref={recaptchaRef}
+              size="invisible"
+              sitekey="6Lfl0tAZAAAAACGkkVOvgt85I_kgTDtBsq0Ht1ch"
+              onChange={onChange}
+            />
           <button type="submit" className="submit-btn btn">submit here</button>
         </form>
       </article>
